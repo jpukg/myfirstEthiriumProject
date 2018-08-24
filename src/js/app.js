0c79f9d56ddb547b7e2c7351a9b0d34087a844d9
jpukg <j.prakashuk@gmail.com>
2018-08-24 20:52:50 +0200

    Initial working version

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,7 @@ App = {
   chairPerson: null,
   currentAccount: null,
   init: function () {
-  
+
     return App.initWeb3();
   },
 
@@ -20,7 +20,7 @@ App = {
     }
     web3 = new Web3(App.web3Provider);
 
-   
+
     return App.initContract();
   },
 
@@ -35,13 +35,13 @@ App = {
 
       App.getClaimStage();
 
-      if(stageGbl === undefined) {
+      if (stageGbl === 0 || stageGbl === undefined) {
         console.log("init ", stageGbl);
         App.registerParties('0xf17f52151ebef6c7334fad080c5704d77216b732', '0xc5fdf4076b8f3a5357c5e395ab970b5b54098fef', '0x821aea9a577a9b44299b9c15c88cf3087f3b5544');
       } else {
         console.log("init else ", stageGbl);
       }
-      
+
       App.getParty();
       App.viewClaim();
       App.viewVoucher();
@@ -54,15 +54,20 @@ App = {
   bindEvents: function () {
     $(document).on('click', '.btn-vote', App.handleVote);
     $(document).on('click', '#win-count', App.handleWinner);
-    // $(document).ready(function () {
-    //   console.log("ready!");
-    //   App.getClaimStage();
-    //   App.getParty();
-    //   App.viewClaim();
-    //   App.viewVoucher();
-    //   App.viewRepair();
 
-    // });
+
+    $(document).on('click', '#claimBtn', function () {
+
+      var policyNo = $('#policyNo').val();
+      var claimDate = $('#claimDate').val();
+      var location = $('#location').val();
+      var claimDescription = $('#claimDesc').val();
+
+      App.handleRegisterClaim(policyNo, claimDate, location, claimDescription);
+
+    });
+
+
 
     $(document).on('click', '.btn-registerClaim', function () {
       var policyNo = $('#policyNo').val();
@@ -75,7 +80,7 @@ App = {
 
     });
   },
- 
+
 
   registerParties: function (_claimant, _provider, _expert) {
     var voteInstance;
@@ -98,8 +103,10 @@ App = {
     App.contracts.vote.deployed().then(function (instance) {
       return instance.getStage();
     }).then(function (result) {
-       console.log("Stage :" , result.toString());
+      // console("Stage :" + result.toString());
+      alert(result.toString());
       stageGbl = result.toString();
+      console.log("result.toString()", result.toString());
       App.chairPerson = result.toString();
       App.currentAccount = web3.eth.coinbase;
       if (App.chairPerson != App.currentAccount) {
@@ -121,17 +128,19 @@ App = {
 
       console.log('View claim', result.toString());
       // var array = JSON.parse("[" + result.toString() + "]");
-      var array = result.toString().split(",").map(Number);
+      var array = result.toString().split(",");
 
       _idPolicyGbl = array[0];
       _dateClaimGbl = array[1];
       _dtLocationGbl = array[2];
       _dtClaimGbl = array[3];
 
-      $("#policyNo").val(_idPolicyGbl);
-      $("#claimDate").val(_dateClaimGbl);
-      $("#location").val(_dtLocationGbl);
-      $("#claimDesc").val(_dtClaimGbl);
+      if (_idPolicyGbl != 0 && _idPolicyGbl != undefined && _idPolicyGbl != "") {
+        $("#policyNo").val(_idPolicyGbl);
+        $("#claimDate").val(_dateClaimGbl);
+        $("#location").val(_dtLocationGbl);
+        $("#claimDesc").val(_dtClaimGbl);
+      }
 
       if (result.receipt.status == '0x01')
         alert(" is registered successfully")
@@ -141,6 +150,7 @@ App = {
       // alert(addr + " account registeration failed")
     })
   },
+
 
   viewVoucher: function () {
     var voteInstance;
@@ -148,9 +158,12 @@ App = {
       voteInstance = instance;
       return voteInstance.viewVoucher();
     }).then(function (result) {
+
       console.log('viewVoucher', result.toString());
-      
-      $("#voucherId").val(result.toString());
+      _idVoucherGbl = result.toString();
+      if (_idVoucherGbl != 0 && _idVoucherGbl != undefined && _idVoucherGbl != "") {
+        $("#voucherId").val(_idVoucherGbl);
+      }
       if (result.receipt.status == '0x01')
         alert(" is registered successfully")
       else
@@ -159,6 +172,7 @@ App = {
       // alert(addr + " account registeration failed")
     })
   },
+
 
   viewRepair: function () {
     var voteInstance;
@@ -168,13 +182,15 @@ App = {
     }).then(function (result) {
       console.log('viewRepair', result.toString());
 
-      var array = result.toString().split(",").map(Number);
+      /*var array = result.toString().split(",").map(Number);*/
+      var array = result.toString().split(",");
 
       _amtRepairGbl = array[0];
       _dtRepairGbl = array[1];
-
-      $("#repairAmount").val(_amtRepairGbl);
-      $("#repairDesc").val(_dtRepairGbl);
+      if (_amtRepairGbl != 0 && _amtRepairGbl != undefined && _amtRepairGbl != "") {
+        $("#repairAmount").val(_amtRepairGbl);
+        $("#repairDesc").val(_dtRepairGbl);
+      }
 
       if (result.receipt.status == '0x01')
         alert(" is registered successfully")
@@ -184,7 +200,6 @@ App = {
       // alert(addr + " account registeration failed")
     })
   },
-
 
   viewExpert: function () {
     var voteInstance;
@@ -193,6 +208,14 @@ App = {
       return voteInstance.viewExpert();
     }).then(function (result) {
       console.log('viewExpert', result.toString());
+
+      _descExpertGbl = result.toString();
+
+      if (_descExpertGbl != 0 && _descExpertGbl != undefined && _descExpertGbl != "") {
+        $("#reqExpert").val(_descExpertGbl);
+      }
+
+
       if (result.receipt.status == '0x01')
         alert(" is registered successfully")
       else
@@ -201,7 +224,6 @@ App = {
       // alert(addr + " account registeration failed")
     })
   },
-
 
   getParty: function () {
     var voteInstance;
@@ -229,7 +251,7 @@ App = {
       return voteInstance.registerClaim(policyNo, claimDate, location, claimDescription);
     }).then(function (result) {
       alert(result);
-      if (result.receipt.status == '0x01')
+      if (result != undefined)
         alert(" is registered successfully")
       else
         alert(" account registeration failed due to revert")
@@ -238,7 +260,7 @@ App = {
     })
   },
 
-   
+
 
 
 };
